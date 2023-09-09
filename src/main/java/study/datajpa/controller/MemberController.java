6b4e15787d6d2f5346703a19f1126277e82bd8a3
dev_one <micool1030@gmail.com>
2023-09-09 10:00:48 +0900

    Web 확장 - 도메인 클래스 컨버터 - 페이징, 정렬

---
+++ src/main/java/study/datajpa/controller/MemberController.java
@@ -0,0 +1,61 @@
+package study.datajpa.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import study.datajpa.dto.MemberDto;
+import study.datajpa.entity.Member;
+import study.datajpa.repository.MemberRepository;
+
+/**
+ * MemberController
+ * <pre>
+ * Describe here
+ * </pre>
+ *
+ * @version 1.0,
+ */
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberRepository memberRepository;
+
+    @GetMapping("/members/{id}")
+    public String findMemberUsername(@PathVariable("id") Long id) {
+        Member member = memberRepository.findById(id).orElseThrow();
+        return member.getUsername();
+    }
+
+    /**
+     * Web 확장 - 도메인 클래스 컨버터
+     * - HTTP 요청은 회원 id를 받지만 도메인 클래스 컨버터가 중간에 동작해서 회원 엔티티 객체를 반환
+     * 도메인 클래스 컨버터도 리파지토리를 사용해서 엔티티를 찾음
+     */
+    @GetMapping("/members2/{id}")
+    public String findMemberUsername(@PathVariable("id") Member member) {
+        return member.getUsername();
+    }
+
+    @GetMapping("/members")
+    public Page<MemberDto> list(@PageableDefault(size = 5) Pageable pageable) {
+        return memberRepository.findAll(pageable)
+                .map(MemberDto::new);
+    }
+
+    // Spring Application 올라올 때 실행 됨 (테스트 데이터 넣는 용도)
+    public void init() {
+        for (int i = 0; i < 100; i++) {
+            memberRepository.save(new Member("user" + i, i));
+        }
+    }
+}
+
+
+
+
--- src/main/java/study/datajpa/dto/MemberDto.java
+++ src/main/java/study/datajpa/dto/MemberDto.java
@@ -1,6 +1,7 @@
 package study.datajpa.dto;
 
 import lombok.Data;
+import study.datajpa.entity.Member;
 
 /**
  * MemberDto
@@ -20,5 +21,10 @@ public class MemberDto {
         this.id = id;
         this.username = username;
         this.teamName = teamName;
+    }
+
+    public MemberDto(Member member) {
+        this.id = member.getId();
+        this.username = member.getUsername();
     }
 }
